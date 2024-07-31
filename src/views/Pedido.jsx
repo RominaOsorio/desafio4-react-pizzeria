a9d8bf675bc005d717b9e7514883e61c8a46c914
Romina Osorio <r.osorio.marmolejo@gmail.com>
2024-07-30 21:14:21 -0400

    modificando responsividad del componente pedido

--- src/views/Pedido.jsx
+++ src/views/Pedido.jsx
@@ -1,51 +1,53 @@
-import { PizzasContext } from '../context/PizzasContext.jsx'
 import { useContext } from 'react'
+import { PizzasContext } from '../context/PizzasContext'
 import { Link } from 'react-router-dom'
 
 const Pedido = () => {
   const { carrito, incrementar, decrementar } = useContext(PizzasContext)
   const total = carrito.reduce(
-    (acumulador, valorActual) =>
-      acumulador + valorActual.price * valorActual.count,
-    0
+    (acumulador, valorActual) => acumulador + valorActual.price * valorActual.count, 0
   )
 
   return (
-    <>
-      <div className='container pt-5'>
-        <h3>Detalle Pedido:</h3>
-        <div className='shadow-lg p-4 mb-5 bg-body-tertiary rounded'>
-          {carrito.map((product, index) => (
-            <div key={product.id} className='flexbox px-5 mb-5'>
-              <div className='flexbox'>
-                <img src={product.img} alt='{product.name}' width={150} />
-                <p>{product.name}</p>
-              </div>
-              <div className='flexbox'>
-                <p>${product.price}</p>
-                <button
-                  className='btn btn-success'
-                  onClick={() => incrementar(index)}
-                >
-                  +
-                </button>
-                <p>{product.count}</p>
-                <button
-                  className='btn btn-danger'
-                  onClick={() => decrementar(index)}
-                >
-                  -
-                </button>
-              </div>
+    <div className='container my-5'>
+      <div className='row shadow body-tertiary rounde p-3'>
+        <h3>Detalle pedido</h3>
+        {carrito.map((product, index) => (
+          <div key={product.id} className='d-flex flex-column flex-md-row '>
+            <div className='col-6 p-3 d-flex gap-5 align-items-center'>
+              <img src={product.img} width={200} />
+              <p className='text-capitalize'>{product.name}</p>
             </div>
-          ))}
-        </div>
-        <h3 className='mb-3'>Total: ${total}</h3>
+            <div className='col-6 p-3 d-flex justify-content-center align-items-center gap-2'>
+              <p>${product.price} </p>
+              <button
+                className='btn btn-success'
+                onClick={() => incrementar(index)}
+              >
+                +
+              </button>
+              <p>{product.count} </p>
+              <button
+                className='btn btn-danger'
+                onClick={() => decrementar(index)}
+              >
+                -
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='row mt-5'>
+        <h3>Total: ${total} </h3>
         <Link to='/'>
-          <button className='btn btn-primary'>Regresar</button>
+          <button
+            className='btn btn-success'
+          >Regresar
+          </button>
         </Link>
       </div>
-    </>
+    </div>
+
   )
 }
 
